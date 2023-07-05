df8193b8f480d643c32b4892ed26c6acafc49965
reza <shekuhireza@gmail.com>
2023-07-05 16:18:30 +0330

    added entitymap

---
+++ EntityMap/ComplexMap.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using ComplexManagment.Entities;
+namespace ComplexManagment.EntityMap
+{
+    public class ComplexMap : IEntityTypeConfiguration<Complex>
+    {
+        public void Configure(EntityTypeBuilder<Complex> builder)
+        {
+            builder.ToTable("Complexs");
+            builder.HasKey(_ => _.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(_=>_.Name).HasMaxLength(50).IsRequired();
+            builder.Property(_=>_.UnitCount).IsRequired();
+        }
+    }
+}
---
+++ EntityMap/UnitMap.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using ComplexManagment.Entities;
+namespace ComplexManagment.EntityMap
+{
+    public class UnitMap : IEntityTypeConfiguration<Unit>
+    {
+        public void Configure(EntityTypeBuilder<Unit> builder)
+        {
+            builder.ToTable("Units");
+            builder.HasKey(_ => _.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(_ => _.Name).HasMaxLength(50).IsRequired();
+            builder.Property(_ => _.BlookId).IsRequired();
+        }
+    }
+}
